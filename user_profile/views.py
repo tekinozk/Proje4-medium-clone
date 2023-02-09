1bbd7df4fe3e4e647b15f0f93b4e62d8c72008b8
Tekin Ozkilic <mtekinozk@gmail.com>
2023-02-10 00:41:42 +0300

    user login progress

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+
 from django.contrib import messages
 
 def login_view(request):
@@ -36,5 +38,35 @@ def logout_view(request):
 def register_view(request):
     context=dict()
     if request.method == "POST":
-        print(request.POST)
+        post_info = request.POST
+        email = post_info.get('email')
+        email_confirm = post_info.get('email_confirm')
+        password = post_info.get('password')
+        password_confirm = post_info.get('password_confirm')
+        first_name = post_info.get('first_name')
+        last_name = post_info.get('last_name')
+        twitter = post_info.get('twitter')
+        if email != email_confirm:
+            messages.warning(request,"Email adresleri Eşleşmedi...")
+            return redirect ("user:register_view")
+        if password != password_confirm:
+            messages.warning(request,"Şifreler Eşleşmedi...")
+            return redirect ("user:register_view")
+        if len(email) < 3 or len(password) < 3 or len(first_name) < 3 or len(last_name) < 3:
+            messages.warning(request,"Minimum karakter sayısını tamamlamalısınız...")
+            return redirect ("user:register_view")
+
+        user,created = User.objects.get_or_create(username=email)
+        if not created:
+            user = authenticate(request,username=email,password=password)
+            if user is not None:
+                messages.warning(request,"Daha önce kayıt olmuşsunuz... Ana Sayfaya Yönlendirildiniz...")
+                login(request,user)
+                return redirect("/")
+        messages.warning(request,"Bu e-posta adresi zaten kayıtlı...")
+        return redirect ("user:login_view")
+
     return render(request,"user_profile/register.html",context)
+            
+
+
