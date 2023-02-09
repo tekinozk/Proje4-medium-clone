15d39f4a78dd5334e631d75b86a8880ba852b469
Tekin Ozkilic <mtekinozk@gmail.com>
2023-02-09 22:50:24 +0300

    login updated

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 
 def login_view(request):
@@ -12,14 +12,23 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
+        if len(username) < 6 or len(password) < 6:
+            messages.warning(request,f"Kullanıcı adınız ya da parolanız geçersiz...")
         user = authenticate(request,username=username,password=password)
-        print(user)
+
         if user is not None:
             login(request,user)
             messages.success(request,f" Heey! {request.user.username} Hoşgeldin!")
             return redirect("home_view")
-        else:
-            messages.warning(request,f" Heey! {request.user.username} Bir şeyler yanlış görünüyor!")
+        elif len(password) > 6 and user==None:
+            messages.warning(request,f"Bir şeyler yanlış görünüyor!")
+
+      
 
 
     return render(request,"user_profile/login.html",context)
+
+def logout_view(request):
+    messages.info(request,f" Heey! {request.user.username} Oturumun Sonlandı")
+    logout(request)
+    return redirect("/")
