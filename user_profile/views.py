95e775d3b16a390d50fb35395176428412f29e5a
Tekin Ozkilic <mtekinozk@gmail.com>
2023-02-09 22:03:55 +0300

    login warning added

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 def login_view(request):
     # login olan user direkt olarak anasayfaya gidebilmeli
     context = dict(
     ) 
+    if request.user.is_authenticated:
+        messages.success(request,f" Heey! {request.user.username} daha önce giriş yapmıştınız")
+        return redirect("home_view")
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -12,7 +16,6 @@ def login_view(request):
         print(user)
         if user is not None:
             login(request,user)
-         
             return redirect("home_view")
 
     return render(request,"user_profile/login.html",context)
